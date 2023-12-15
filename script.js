272cee37a4d10fa89bb588509c3f06381aecc57c
OceanGonen <ocean.gonen@hotmail.com>
2023-12-15 14:32:19 +0100

    update-0.2
    added ul and li to nav
    css clean up

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var backToTopButton = document.getElementById('top');
+  var backToTopButton = document.getElementById('top');
 
-    backToTopButton.classList.add('show');
-  
-    function scrollToTop() {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-      });
-    }
-  
-    backToTopButton.addEventListener('click', scrollToTop);
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  backToTopButton.addEventListener('click', scrollToTop);
 });
